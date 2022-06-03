d42a929504c68ec238531204fcf17dfd42dc16f8
mitani24 <h.mitani@ashita-team.com>
2022-06-03 19:16:32 +0900

    urql setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import { Provider } from "urql";
+import { client } from "../lib/urql-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
 
 export default MyApp;
